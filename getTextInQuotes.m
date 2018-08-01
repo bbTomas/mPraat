d25258edead142164a937daa086dd2438c340f06
bbTomas <borilt@gmail.com>
2018-08-01 18:34:19 +0200

    match empty strings

--- getTextInQuotes.m
+++ getTextInQuotes.m
@@ -1,7 +1,8 @@
 function text = getTextInQuotes(str)
 % Author: Pol van Rijn
+% ".+?" -> ".*?"  by Tomas Boril
 
-    text = regexp(str, '".+?"', 'match'); % find quoted text
+    text = regexp(str, '".*?"', 'match'); % find quoted text
     text = strrep(text, '"', ''); % remove quotes
     text = text{1}; % remove cell around string
 end
